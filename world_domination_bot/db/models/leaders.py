1c0d9be8bfe1c4e53ef79c3efd1e86b17f836ba7
Anton Semeniuta <antonsemenyuta@yandex.ru>
2023-02-12 23:10:39 +0700

    Added user_id to leader model and new test country: China

--- world_domination_bot/db/models/leaders.py
+++ world_domination_bot/db/models/leaders.py
@@ -10,7 +10,7 @@ from db.base_class import Base
 
 class Leader(Base):
     name = Column(String, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False, unique=True)
     country_name = Column(Integer, ForeignKey('country.name'))
     session_id = Column(Integer, ForeignKey('session.id'))
     country = relationship('Country', back_populates='leader')
