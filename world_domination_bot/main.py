a4639dc2d2822365fc6572e99390faf0f4aa558c
Anton Semeniuta <antonsemenyuta@yandex.ru>
2023-02-16 01:04:10 +0700

    Actualized main.py with new function and class names

--- world_domination_bot/main.py
+++ world_domination_bot/main.py
@@ -16,27 +16,26 @@ if ROOT_DIR not in sys.path:
     sys.path.append(str(ROOT_DIR))
 
 
-from callbacks.callback_lib import get_callbacks
 from common.config import TO_GENERATE_AND_DROP_DB
 from common.config import TOKEN_API
+from common.fake_data_generation_lib import generate_test_session
 from common.logger import setup_logging
-from db.models.sessions import Session
 from db.db_lib import create_tables
 from db.db_lib import drop_tables
+from db.models.game_sessions import GameSession
 from db.session import get_db
-from db.test_functions.generate_test_leader import generate_test_leader
 from handlers.handlers import register_handlers
 
 
-async def on_startup(_, db: Session = next(get_db())):
+async def on_startup(_, db: GameSession = next(get_db())):
     """"""
     logger.info('Bot was successfully launched')
     if TO_GENERATE_AND_DROP_DB:
-        generate_test_leader(db)
+        generate_test_session(db)
     logger.info('Test data was generated successfully.')
 
 
-async def on_shutdown(dp: Dispatcher, db: Session = next(get_db())):
+async def on_shutdown(dp: Dispatcher, db: GameSession = next(get_db())):
     logging.warning('Shutting down..')
     if TO_GENERATE_AND_DROP_DB:
         drop_tables()
