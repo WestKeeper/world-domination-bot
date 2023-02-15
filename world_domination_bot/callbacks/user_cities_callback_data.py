e04d21c7c30c2d5295ac07e0a2f634e9a1e5c20b
Anton Semeniuta <antonsemenyuta@yandex.ru>
2023-02-16 00:57:07 +0700

    Deleted some files with old names to rename

--- world_domination_bot/callbacks/user_cities_callback_data.py
+++
@@ -1,7 +0,0 @@
-from aiogram.utils.callback_data import CallbackData
-
-
-def get_user_cities_callback_data() -> CallbackData:
-    """"""
-    cb = CallbackData('user_city', 'city_name')
-    return cb
--- world_domination_bot/states/game_states_group/game_states_group.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher.filters.state import State
-from aiogram.dispatcher.filters.state import StatesGroup
-
-
-class GameStatesGroup(StatesGroup):
-    """"""
-    order = State()
